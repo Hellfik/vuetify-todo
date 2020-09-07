a05b31f62f16b98eb7a07eba06ec5eb2c2482bb0
Mika Fayeulle <mika.fayeulle@gmail.com>
2020-09-07 17:18:56 +0200

    Added Navigation Component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav>
+        <v-app-bar flat dark app>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo </span>
+                <span>Ninja</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey">
+                <span>Sign in</span>
+                <v-icon right>
+                    mdi-logout
+                </v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" dark app>
+            <v-list v-for="(link, index) in links" :key="index">
+                <v-list-item router :to="link.path">
+                    <v-list-item-avatar>
+                        <v-icon class="white--text">
+                            {{ link.icon}}
+                        </v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer: false,
+            links: [
+                {icon: 'mdi-home', text: 'Dashboard', path: '/'},
+                {icon: 'mdi-folder', text: 'My projects', path: '/projects'},
+                {icon: 'mdi-account', text: 'Team', path: '/team'},
+                
+            ],
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+    <!-- <v-btn class="pink white--text">Click me!</v-btn><br><br>
+    <v-btn color="pink" depressed>Click me!</v-btn><br><br>
+    <v-btn color="pink" outlined>Click me!</v-btn><br><br>
+
+    <v-btn color="pink" dark depressed>
+      <v-icon left>mdi-phone</v-icon>
+      <span>Call me</span>
+    </v-btn>
+    <br>
+    <br>
+    <v-btn color="pink" small dark depressed>
+      <v-icon left small>mdi-phone</v-icon>
+      <span>Call me</span>
+    </v-btn>
+    <br>
+    <br>
+    <v-btn color="pink" large dark depressed>
+      <v-icon left large>mdi-phone</v-icon>
+      <span>Call me</span>
+    </v-btn>
+    <br>
+    <br>
+    <v-btn fab color="purple" small dark depressed>
+      <v-icon small>mdi-heart</v-icon>
+    </v-btn> -->
+
+    <!-- <v-btn class="hidden-md-and-down">Click me</v-btn> -->
+    <!-- <v-btn class="hidden-md-and-up">Click me</v-btn> -->
+    <v-btn class="hidden-sm-only">Click me</v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
